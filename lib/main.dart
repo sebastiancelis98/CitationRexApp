350adf0a25fc1dc75dcfc64ce7db30c00970c8c4
sebastiancelis98 <celis.sebastian@yahoo.com>
2020-05-19 16:59:04 +0200

    Update app title in metadata

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CitationRex',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
