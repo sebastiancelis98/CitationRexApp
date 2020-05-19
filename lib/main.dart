5dc25d30bf08bd8d5ecbd960ef518f6a7218b78b
vinzenzzinecker98 <vinzenz@zinecker.de>
2020-05-19 16:50:47 +0200

    update: added button

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+git 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Citation Rex',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -31,9 +31,50 @@ class RootPage extends StatelessWidget {
         body: Stack(
       children: <Widget>[
         Expanded(
-          child: Text('Hello world'),
-        )
+          child: Text('welcome to the Citation Recommendation demo'),
+        ),
+        TextField(
+        decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: 'Enter the citation context here'
+        ),
+        ),
+        const SizedBox(height: 30),
+        MyButton(),
+
+      
       ],
     ));
+  }
+}
+class MyButton extends StatefulWidget {
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  String buttontext = "GO";
+  @override
+
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text(buttontext),
+      onPressed: () {
+        
+        print("ohhimarkus");
+        if (buttontext=="GO"){
+          buttontext="HI";
+        }
+        else {
+          buttontext="GO";
+        }
+        setState(() {
+          
+        });
+
+        
+        
+        },
+    );
   }
 }
